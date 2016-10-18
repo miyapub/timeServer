fe81b44f1738b2c35b8a72cc02c25f72249603ae
miya <miyapub@outlook.com>
2016-10-18 18:01:49 +0800

    up

--- handlers/color_random.js
+++ handlers/color_random.js
@@ -1,11 +1,5 @@
 var tinycolor = require('tinycolor2');
-function num() {
-    return Math.floor(Math.random() * 255);
-}
 module.exports = function (request, reply) {
-    var r = num();
-    var g = num();
-    var b = num();
-    var color = tinycolor({ r: r, g: g, b: b });
+    var color = tinycolor.random();
     return reply(color.toHex());
 }
